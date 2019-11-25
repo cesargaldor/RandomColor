771beea72a83fbdc867113fd3608f2fb39cf6ea5
César <cesargaldor@gmail.com>
2019-11-25 01:15:35 +0100

    Done

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <colors />
+  </div>
+</template>
+
+<script>
+import colors from "./components/colors.vue";
+
+export default {
+  name: "app",
+  components: {
+    colors
+  }
+};
+</script>
+
+<style>
+* {
+  font-family: "Kulim Park", sans-serif;
+  font-weight: 300;
+}
+</style>
---
+++ src/components/boton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="button">
+    <button class="btn" onClick="window.location.reload();">Get color</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "boton"
+};
+</script>
+
+<style>
+.btn {
+  text-transform: uppercase;
+  font-size: 0.8em;
+  text-decoration: none;
+  background: #fff;
+  color: #000;
+  padding: 10px;
+  border-radius: 50px;
+  display: inline-block;
+  border: none;
+  transition: all 0.4s ease 0s;
+  outline: none;
+  cursor: pointer;
+  width: 10em;
+}
+
+.btn:hover {
+  background: #434343;
+  color: white;
+  letter-spacing: 1px;
+  -webkit-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+  box-shadow: 5px 40px -10px rgba(0, 0, 0, 0.57);
+  transition: all 0.4s ease 0s;
+}
+</style>
